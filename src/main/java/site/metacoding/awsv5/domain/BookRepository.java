042efd7e57e0d633a0d9a2d05400505a8d81f6fb
sylar0012 <kmc42692@naver.com>
2022-11-08 10:25:32 +0900

    Upstream test

--- src/main/java/site/metacoding/awsv5/domain/BookRepository.java
+++ src/main/java/site/metacoding/awsv5/domain/BookRepository.java
@@ -3,5 +3,5 @@ package site.metacoding.awsv5.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+  // asd
 }
